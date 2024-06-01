d1a166063ce72bc458a3f50b6df0778062a75afa
Suhail2701 <suhail.ahmed2701@gmail.com>
2024-06-01 18:42:07 +0530

    first commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 const h1 = React.createElement('h1', {
-    id:"header", xyx:"suahil"
+    id:"header", xyx:"suahil", class:"head"
 }, 'Hello world');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
