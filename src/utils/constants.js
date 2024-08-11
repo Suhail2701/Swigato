171ea87870e427d18c3e3f27d913e50486f31505
Suhail Ahmed <92672136+Suhail2701@users.noreply.github.com>
2024-08-12 00:55:49 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,6 @@ export const LOGO_URL = "https://webstockreview.net/images/clipart-restaurant-re
 
 export const CART_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-// export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId=";
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0759837&lng=72.8776559&restaurantId=";
 
 export const NO_RESULT_IMG = "https://cdn.dribbble.com/users/1554526/screenshots/3399669/no_results_found.png";
@@ -11,6 +10,4 @@ export const AI_API_KEY = "sk-UlV7XwynN8GFuewzFyOXb1zKwwsbUxDjJRBHJYoycvT3BlbkFJ
                          
 export const AI_API_KEY2 = "sk-go_8-T53QwEyzxoNIxDRccKi4hOfREeM8GxZOOra_ST3BlbkFJxQZWiXwi2FyZabXbxPLuR-1_Vsb24RzZnKxT9RbM8A";
 
-
-// export const PROXY_URL = "https://thingproxy-760k.onrender.com/fetch/";
-export const PROXY_URL = "https://thingproxy.freeboard.io/fetch/";
+export const PROXY_URL = "https://thingproxy-760k.onrender.com/fetch/";
